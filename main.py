efc7d2f77df51bb1ffb9c281b0de952dec509a7f
Hong Guan (Student) <hguan6@en4217549l.fulton.ad.asu.edu>
2022-10-31 16:50:03 -0700

    change pickle model to xgboost; add more test instances; use bitarray to hold bitvectors

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 from host import Host
 from client import Client
-
+from load_tree import parse_from_pickle
+import pandas as pd
+import numpy as np
 
 def test():
-
-    feature_division = [tuple(range(0, 16)), tuple(range(16, 24)), tuple(range(24, 29))]
-
-    def federated_inference(feature):
-        forest, _ = parse_from_pickle()
-
+    def federated_inference(forest, feature, feature_division):
         client1 = Client()
         client2 = Client()
         host = Host()
-
-        features = pd.read_csv(
-            "test_utils/test_data/test_samples.csv",
-            dtype=np.float32,
-            usecols=range(1, 29),
-            header=None,
-        )
 
         host.local_compute()
         bitvectorss_from_client1 = client1.local_compute()
         bitvectorss_from_client2 = client2.local_compute()
         return host.aggregate([bitvectorss_from_client1, bitvectorss_from_client2])
 
+    feature_division = [tuple(range(0, 16)), tuple(range(16, 24)), tuple(range(24, 29))]
+    forest, sklearn_model = parse_from_pickle() 
     features = pd.read_csv(
         "test_utils/test_data/test_samples.csv",
         dtype=np.float32,
@@ -33,7 +25,7 @@ def test():
     )
 
     for feature in features:
-        prediction1 = federated_inference(forest, feature)
+        prediction1 = federated_inference(forest, feature, feature_division)
         prediction2 = int(sklearn_model.predict())
         assert (
             prediction1 == prediction2
